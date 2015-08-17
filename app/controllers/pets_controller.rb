4f49640564613cbe88df7cd6b34337f502cc9ce5
jnf <jeremy.flores@gmail.com>
2015-08-17 08:42:39 -0700

    sample code in case everything goes wrong

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,16 @@
 class PetsController < ApplicationController
+  def index
+    pets = Pet.all
+    render json: pets.as_json(except: [:created_at, :updated_at]), status: :ok
+  end
+
+  def show
+    pet = Pet.find_by(id: params[:id])
+    
+    if pet
+      render json: pet.as_json(except: [:created_at, :updated_at]), status: :ok
+    else
+      render json: [], status: 204
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/pets' => 'pets#index'
+  get '/pets/:id' => 'pets#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
