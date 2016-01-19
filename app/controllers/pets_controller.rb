0554c7053ed522c60edee812ad0a1f9cce85ca16
Kari Bancroft <kariabancroft@gmail.com>
2016-01-19 10:01:13 -0800

    Add live code stuff

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,10 @@
 class PetsController < ApplicationController
+  def index
+    pets = Pet.all
+    render :json => pets.as_json(except: [:created_at, :updated_at]),
+      :status => :ok
+  end
+
+  def show
+  end
 end
